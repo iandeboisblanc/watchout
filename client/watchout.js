76446a7496f6049edb994e13926cda517749c413
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-22 15:07:56 -0800

    Draws svg rectangle and displays board
    Gets width and height properties from watchout.js

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,21 @@
 // start slingin' some d3 here.
+var gameOptions = { 
+  height: '450px',
+  width: '700px',
+  nEnemies: '30px',
+  padding: '20px'
+};
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+};
+
+var gameBoard = d3.select('.board').append('svg:svg')
+                .attr('width', gameOptions.width)
+                .attr('height', gameOptions.height);
