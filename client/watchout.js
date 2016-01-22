223586271bc64c342ddcba202e442afee194f210
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-22 15:28:07 -0800

    Add circle elements to svg gameboard based on enemies dataset

--- client/watchout.js
+++ client/watchout.js
@@ -18,4 +18,15 @@ var axes = {
 
 var gameBoard = d3.select('.board').append('svg:svg')
                 .attr('width', gameOptions.width)
-                .attr('height', gameOptions.height);
+                .attr('height', gameOptions.height)
+                .attr('class','gameBoard');
+
+var enemies = [[10,10],[20,40],[40,80]];
+
+d3.select('.gameBoard').selectAll('circle')
+  .data(enemies).enter()
+  .append('circle').attr('class','enemy')
+  .attr('cx', function(d){return axes.x(d[0]);})
+  .attr('cy', function(d){return axes.y(d[1]);})
+  .attr('r', 5); //could come back and make styling data-dependent
+
