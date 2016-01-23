b7a2c234e9abc54e4ae89943d88a15eb82d888a2
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-23 11:52:39 -0800

    Redesigned enemies to be shurikens

--- client/watchout.js
+++ client/watchout.js
@@ -9,6 +9,13 @@ var gameOptions = {
   enemyRadius: 15
 };
 
+String.prototype.px2num = function() {
+  return +this.slice(0,-2);
+};
+
+gameOptions.numHeight = gameOptions.height.px2num();
+gameOptions.numWidth = gameOptions.width.px2num();
+
 var gameStats = {
   score: 0,
   bestScore: 0,
@@ -20,9 +27,14 @@ var axes = {
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
 };
 
+var numericAxes = {
+  x: function(val) { return axes.x(val).px2num(); },
+  y: function(val) { return axes.y(val).px2num(); },
+};
+
 var invertAxes = {
-  x: d3.scale.linear().domain([0,+gameOptions.width.slice(0,-2)]).range([0,100]),
-  y: d3.scale.linear().domain([0,+gameOptions.height.slice(0,-2)]).range([0,100])
+  x: d3.scale.linear().domain([0,gameOptions.numWidth]).range([0,100]),
+  y: d3.scale.linear().domain([0,gameOptions.numHeight]).range([0,100])
 };
 
 var random = Math.random;
@@ -31,25 +43,29 @@ var max = Math.max;
 var min = Math.min;
 var sqrt = Math.sqrt;
 
+
 var gameBoard = d3.select('.board').append('svg:svg')
                 .attr('width', gameOptions.width)
                 .attr('height', gameOptions.height)
                 .attr('class','gameBoard');
 
-var enemies = [ {x:10, y:10},
-                {x:20, y:40},
-                {x:40, y:80},
+d3.select('.centered')
+  .style('width', gameOptions.width);
+
+var enemies = [ {x:10, y:10, colliding:false},
+                {x:20, y:40, colliding:false},
+                {x:40, y:80, colliding:false},
               ];
 
 var drag = d3.behavior.drag()
     .on("drag", function(d){
       var x = d3.event.x;
       var y = d3.event.y;
-      d.x = invertAxes.x(x);
-      d.y = invertAxes.y(y);
+      d.x = max(0, min(invertAxes.x(x), 100));
+      d.y = max(0, min(invertAxes.y(y), 100));
       d3.select(this)
-      .attr("cx", max(d.r, min(+gameOptions.width.slice(0,-2) - d.r, x)))
-      .attr("cy", max(d.r, min(+gameOptions.height.slice(0,-2) - d.r, y)));
+      .attr("cx", max(d.r, min(gameOptions.numWidth - d.r, x)))
+      .attr("cy", max(d.r, min(gameOptions.numHeight - d.r, y)));
     });
 
 var player = [{x:50, y:50, r:gameOptions.playerRadius}];
@@ -77,26 +93,30 @@ var tweenWithCollisionDetection = function(endPosition) {
   updatePosition(enemy.datum());
   return function(t) {
     //check for collisions
-    checkForCollisions(enemy);
+    if(!enemy.colliding){
+      checkForCollisions(enemy);
+    }
     //have new positions
     var midX = startX + (endPosition.x - startX) * t;
     var midY = startY + (endPosition.y - startY) * t;
     //set attr
-    enemy.attr('cx', axes.x(midX))
-          .attr('cy', axes.y(midY));
+    enemy.attr('x', axes.x(midX))
+          .attr('y', axes.y(midY));
   };
 };
 
 var checkForCollisions = function (enemy) {
   var playerD = d3.select('.player').datum();
-  var enemyD = {x: +(enemy.attr('cx').slice(0,-2)),
-                y: +(enemy.attr('cy').slice(0,-2))}; //calc distance
-  var distance = sqrt(pow(+(axes.x(playerD.x).slice(0,-2) - enemyD.x),2) + pow(+(axes.y(playerD.y).slice(0,-2) - enemyD.y),2));
+  var enemyD = {x: enemy.attr('x').px2num(),
+                y: enemy.attr('y').px2num()}; //calc distance
+  var distance = sqrt(pow((numericAxes.x(playerD.x) - enemyD.x),2) + pow((numericAxes.y(playerD.y) - enemyD.y),2));
   //if distance < sum of radii
   if(distance < gameOptions.playerRadius + gameOptions.enemyRadius) {
     gameStats.collisions++;
     gameStats.bestScore = max(gameStats.bestScore, gameStats.score);
     gameStats.score = 0;
+    enemy.colliding = true;
+    setTimeout(function(){enemy.colliding = false;}, 500);
   }
 };
 
@@ -105,11 +125,13 @@ var update = function() {
   var enemiesSelection = d3.select('.gameBoard').selectAll('.enemy')
     .data(enemies);
   enemiesSelection.enter()
-    .append('circle')
+    .append('image')
     .attr('class','enemy')
-    .attr('cx', function(d) { return axes.x(d.x); })
-    .attr('cy', function(d) { return axes.y(d.y); })
-    .attr('r', gameOptions.enemyRadius); //could come back and make styling data-dependent
+    .attr('x', function(d) { return axes.x(d.x); })
+    .attr('y', function(d) { return axes.y(d.y); })
+    .attr('xlink:href','shuriken.png')
+    .attr('height', 2*gameOptions.enemyRadius)
+    .attr('width', 2*gameOptions.enemyRadius);
   enemiesSelection
     .transition()
     .duration(gameOptions.stepInterval)
@@ -127,3 +149,26 @@ setInterval(function() {
     .data([gameStats.bestScore, gameStats.score, gameStats.collisions])
     .text(function(d){return d;});
 },gameOptions.stepInterval/10);
+setInterval(function(){
+  d3.select('.gameBoard')
+    .data(player)
+    .transition()
+    .duration(1)
+    .style('background-color', function(d){
+      var r = sqrt(pow(d.x - 50,2) + pow(d.y - 50,2)) * 250/(50*sqrt(2));
+      var g = d.x*250/100;
+      var b = d.y*250/100;
+      return 'rgb(' + r + ',' + g + ',' + b +')';
+    });
+}, 1);
+
+
+
+
+
+
+
+
+
+
+
